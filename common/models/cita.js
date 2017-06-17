fa7436ec2f9816508327a357d8fd3895647e19a0
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2017-06-17 00:31:10 -0500

    mas endpoints

--- common/models/cita.js
+++ common/models/cita.js
@@ -1,5 +1,28 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = function(Cita) {
 
+    Cita.beforeRemote('create', function(ctx, instance, next) {
+       var body = ctx.args.data;
+       var now = new Date();
+       var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+       body.created = now_utc;
+       next(); 
+    });
+
+    Cita.beforeRemote('find', function(ctx, instance, next) {
+        if (!ctx.args.filter) {
+            ctx.args.filter = {};
+        }
+        ctx.args.filter.include = [ "recojerubicacion","agente","propiedads" ];
+        if (!ctx.args.filter.where) {
+            ctx.args.filter.where = {};
+        }
+        ctx.args.filter.where = _.merge(ctx.args.filter.where);
+        ctx.args.filter.order = "created DESC"
+        next();
+    });
+   
 };
--- common/models/person.js
+++ common/models/person.js
@@ -1,5 +1,28 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = function(Person) {
+
+    Person.beforeRemote('create', function(ctx, instance, next) {
+       var body = ctx.args.data;
+       var now = new Date();
+       var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+       body.created = now_utc;
+       next(); 
+    });
+
+    Person.beforeRemote('find', function(ctx, instance, next) {
+        if (!ctx.args.filter) {
+            ctx.args.filter = {};
+        }
+        ctx.args.filter.include = [ "messages","agente" ];
+        if (!ctx.args.filter.where) {
+            ctx.args.filter.where = {};
+        }
+        ctx.args.filter.where = _.merge(ctx.args.filter.where);
+        ctx.args.filter.order = "created DESC"
+        next();
+    });
 
 };
